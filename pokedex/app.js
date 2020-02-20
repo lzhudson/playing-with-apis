49eea4fb75dbd7900da37a4a1d15312968a2d92f
Hudson Holanda <hudsonholanda55@gmail.com>
2020-02-19 21:51:23 -0300

    adicionando função para filtrar por tipo e por nome.

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,15 +1,15 @@
 const pokedex = document.getElementById('pokedex')
 
-const pokemon = document.getElementById('pokemon');
+const pokemon = document.getElementById('pokemon-name');
 
+const selectTypeEl = document.querySelector('#select-type');
 
 const fetchPokemon = () => {
   let loader = `<div class="loader"></div>`;
   document.getElementById('pokedex').innerHTML = loader;
   const promises = [];
   for(let i = 1; i <= 150; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`
     promises.push(fetch(url).then(res => res.json()))
   }
   Promise.all(promises).then(results => {
@@ -20,27 +20,33 @@ const fetchPokemon = () => {
         type: data.types.map(type => type.type.name).join(', ')
     }))
     displayPokemon(pokemon);
-  })
-    
-  const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-
-    const pokemonHTMLString = pokemon.map(pokemon=> `
-      <li class="card">
-        <img class="card-image" src="${pokemon.image}"/>
-        <h2 class="card-title">${pokemon.id} . ${pokemon.name}</h2>
-        <p class="card-subtitule">Type: ${pokemon.type}</p>
-      </li>
-    `
-    ).join('');
-    pokedex.innerHTML = pokemonHTMLString;
-
-  }
-  
-  
+  });
 };  
 
-const filterPokemon = (e) => {
+const displayPokemon = (pokemon) => {
+  pokedex.style.justifyContent = 'flex-start';
+
+  const pokemonHTMLString = pokemon.map(pokemon=> `
+    <li class="card">
+      <img class="card-image" src="${pokemon.image}"/>
+      <h2 class="card-title">${pokemon.id} . ${pokemon.name}</h2>
+      <p class="card-subtitule">Type: ${pokemon.type}</p>
+    </li>
+  `
+  ).join('');
+  pokedex.innerHTML = pokemonHTMLString;
+
+}
+
+const fetchTypes = async (e) => {
+  const fetchTypes = await fetch(`http://pokeapi.co/api/v2/type`);
+
+  const responseData = await fetchTypes.json();
+
+  return responseData.results;
+}
+
+const filterPokemonName = (e) => {
   const text = e.target.value.toLowerCase();
   document.querySelectorAll('.card').forEach(card => {
     const itemText = card.firstElementChild.nextElementSibling.textContent;
@@ -53,7 +59,39 @@ const filterPokemon = (e) => {
   })
 }
 
+const createTypesOptions = (data) => {
+  data.forEach(type => {
+    const optionEl = document.createElement('option');
+    const valueOption = type.name;
+    const textValue = type.name;
+    optionEl.appendChild(document.createTextNode(textValue));
+    optionEl.setAttribute('value', valueOption);
+    selectTypeEl.appendChild(optionEl);
+  })
+}
 
+
+
+fetchTypes()
+  .then(data => {
+    createTypesOptions(data);
+  });
+
+const filterPokemon = (e) => {
+  const optionValue = e.target.value;
+  document.querySelectorAll('.card').forEach(card => {
+    const typePokemonCard = card.querySelector('.card-subtitule').textContent.substring(6,);
+    if(typePokemonCard.includes(optionValue)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  })
+}
 
 fetchPokemon();
-pokemon.addEventListener('keyup', filterPokemon);
+
+selectTypeEl.addEventListener('change', filterPokemon);
+
+pokemon.addEventListener('keyup', filterPokemonName);
+
